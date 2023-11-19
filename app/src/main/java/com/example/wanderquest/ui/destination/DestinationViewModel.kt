f2856914444a57f9a02e7d27c645db05dc872cd7
mspriyakk <mspriyakk@gmail.com>
2023-11-19 15:10:22 -0500

    remove my api key

--- app/src/main/java/com/example/wanderquest/ui/destination/DestinationViewModel.kt
+++ app/src/main/java/com/example/wanderquest/ui/destination/DestinationViewModel.kt
@@ -10,7 +10,7 @@ class DestinationViewModel: ViewModel () {
     //write get
     fun getDestination(){
         viewModelScope.launch {
-            val result = DestinationApi.retrofitService.getDestination("restaurants in New York", "AIzaSyCRPyR0FILxgbwgLDfgRMzU3zohQnnKaRE");
+            val result = DestinationApi.retrofitService.getDestination("restaurants in New York", "");
             Log.d("hooray ", result.toString())
         }
     }
